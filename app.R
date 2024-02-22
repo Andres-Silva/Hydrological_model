290c44c2f6d388a0e5fca8371b624c13eb480773
Andres-Silva <andresslvdq@gmail.com>
2024-02-22 18:12:14 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -3,11 +3,15 @@
 # the 'Run App' button above.
 #
 
+packages <- c('shiny', 'leaflet', 'sp',"raster","whitebox",
+              "elevatr","stars","tidyverse","tools","vroom","rgdal",
+              "htmltools","patchwork","bslib","waiter","scales")
+
+install.packages(setdiff(packages,
+                         rownames(installed.packages())))
 
 require(leaflet)
 require(shiny)
-require(leaflet.extras)
-require(leaflet.extras2)
 require(sp)
 require(raster)
 require(whitebox)
@@ -18,39 +22,83 @@ require(tools)
 require(vroom)
 require(rgdal)
 require(htmltools)
-require(tidyverse)
 require(patchwork)
-
-setwd("~/Library/CloudStorage/GoogleDrive-ahsilvad@unal.edu.co/My Drive/Invest_model/model_App/Invest_model")
-
+require(bslib)
+require(waiter)
+require(scales)
 
 source(paste0(getwd(),"/functions/watershed_function2.R"))
 source(paste0(getwd(),"/functions/invest_model.R"))
 source(paste0(getwd(),"/functions/TWI_function.R"))
 
+
+
 shinyApp(
  ui<-fluidPage(
-  sidebarLayout(
-   sidebarPanel(textInput(inputId = "value",label = "Densidad (n/ha)",),
-                fileInput(inputId = "upload",
-                          label =  "Upload a file",
-                          accept = c(".kml")),
-                downloadButton("download")),
-  mainPanel(#plotOutput("myplot")
-   leafletOutput("map",width = "850", height = "850")
-   ))),
+  waiter::use_waiter(),
+  theme = bs_theme(bootswatch = "flatly"), 
+  titlePanel("Invest Model",windowTitle = "Invest Model"),
+  tabsetPanel(
+   tabPanel("Inicio",
+            
+    sidebarLayout(
+     sidebarPanel(
+      textInput(inputId = "value",label = "Densidad (n/ha)"),
+      fileInput(inputId = "upload", label =  "Seleccionar archivo",
+                accept = c(".kml")),
+      sliderInput(inputId = "nsim",
+                  label =  "Porcentaje de cambio de cobertura", 
+                  min = 0, max = 100, value = 1,step = 10)),
+    mainPanel(
+     leafletOutput("map",width = "1000", height = "850")
+   )
+    )
+   ),
+  tabPanel("Resultados",
+           mainPanel(
+           plotOutput(outputId = "myplot",width =600, 
+                      height = 650),
+           p(
+            outputId = "text",
+             p("La herramienta para la cuantificación de servicios ecosistémicos 
+        utilizados por el conjunto de modelos InVest aborda el balance hídrico 
+        de una cuenca hidrográfica  mediante la función de Budyko,
+        la cual relaciona el índice evaporativo (radio entre 
+        la evapotranspiración real y precipitación) con el índice de aridez 
+        (radio entre la evapotranspiración potencial y 
+        evapotranspiración real). En este aplicativo Web se hace una 
+        aproximación a partir de fuentes secundarias de información sobre
+        la incidencia de los cultivos de aguacate en las demanda hídrica de 
+        una cuenca y en el almacenamiento de carbono en la biomasa aérea viva.
+        El aplicativo entrega información sobre el balance hídrico en escalas de
+        tiempo mensuales para la cobertura actual y una simulación en la cual un
+        porcentaje de bosque es reemplazada cultivos de aguacate.Los parámetros 
+        de entrada hacen referencia a la densidad de siembra del cultivo de interés 
+        por hectárea. La delimitación del área del cultivo (formato .kml) y 
+        el porcentaje de bosque que será reemplazado cultivos de aguacate"), )
+           ),
+           downloadButton("download")
+           
+  )
+  )
+  ),
  
  server<-function(input,output){
   
   map<-reactive({
   req(input$upload)
   req(input$value) 
+  req(input$nsim)
+  
   map <- read_sf(input$upload$datapath[1])
   })
   
   
   #Reactive watershed delimitation
   invest_model<-reactive({
+   waiter <- waiter::Waiter$new()
+   waiter::Waiter$new(id = "myplot")$show()
+   
    
    layer<-map() 
    coords<-layer %>% st_zm() %>% 
@@ -104,22 +152,22 @@ shinyApp(
    #Kc approximation from sentinel landcover
    
    landcover_crop<-crop(landcover,w)
+   
    landcover_crop<-
-    calc(landcover_crop,fun = function(x){ifelse(x %in% c(10,30,40,20),x,
-                                                 NA)})
+    calc(landcover_crop,fun = function(x){
+     ifelse(x %in% c(10,30,40,20),x,  NA)})
    
    labels<-c(10,30,40,20)
    kc_values<-c(1.25,0.8,0.7,0.8)
    
+   landcover_crop<-resample(landcover_crop,dem,
+                            method = 'ngb')
+   
    kc_data<-data.frame(labels,kc_values)
    
-   for(i in 1:nrow(kc_data)){
-    values(landcover_crop)[
-     values(landcover_crop)==kc_data[i,1]]<-kc_data[i,2]
-   }
+   kc<-reclassify(landcover_crop,
+                              rcl = kc_data)
    
-   
-   kc<-resample(landcover_crop,dem,method = "ngb")
    
    pr_roi<-resample(pr_roi,dem)*0.1
    pet_roi<-resample(pet_roi,dem)*0.01
@@ -132,9 +180,9 @@ shinyApp(
    
    awc_crop<-crop(awc,w)/100
    
-   awc_layer<-(1/2)*((50-0)*(awc_crop$WWP_M_sl1_250m_ll+awc_crop$WWP_M_sl2_250m_ll)+
-                      (150-50)*(awc_crop$WWP_M_sl2_250m_ll+awc_crop$WWP_M_sl3_250m_ll)+
-                      (300-150)*(awc_crop$WWP_M_sl3_250m_ll+awc_crop$WWP_M_sl4_250m_ll))
+   awc_layer<-(1/2)*((50-0)*(awc_crop$layer.1+awc_crop$layer.2)+
+                      (150-50)*(awc_crop$layer.2+awc_crop$layer.3)+
+                      (300-150)*(awc_crop$layer.3+awc_crop$layer.4))
    
    awc_layer<-resample(awc_layer,dem)
    
@@ -148,22 +196,22 @@ shinyApp(
    #Random Kc selection
    layer2<-st_zm(layer)
    
+   
+   area<-as.numeric(st_area(layer2))/10000
+   
    cells_positions<-raster::extract(kc,as_Spatial(layer2),
                                     cellnumbers=T)
    
    values(kc)[cells_positions[[1]][,1]]<-0.75
    
-   sim_values<-c(0.5,0.75,1)
-   kc_layers<-stack(kc)
-   
-   for(i in 1:length(sim_values)){
-    kc_sim<-kc
-    cell_target<-which(values(kc_sim) == 1.25)
-    n_cells<-length(cell_target)*sim_values[[i]]
-    random_cells<-sample(cell_target,size = round(n_cells))
-    values(kc_sim)[random_cells] = 0.75
-    kc_layers<-addLayer(kc_layers,kc_sim)
-    }
+   sim_values<-as.numeric(input$nsim)/100
+
+   kc_sim<-kc
+   cell_target<-which(values(kc_sim) == 1.25)
+   n_cells<-length(cell_target)*sim_values
+   random_cells<-sample(cell_target,size = round(n_cells))
+   values(kc_sim)[random_cells] = 0.75
+   kc_layers<-addLayer(kc,kc_sim)
    
    
    df<-NULL
@@ -188,52 +236,56 @@ shinyApp(
   
    
    df$type<-factor(df$type,levels = unique(df$type),
-                   labels = c("Actual","50%","75%","100%"),ordered = T)
+                   labels = c("Actual",paste0(input$nsim,"%")),
+                   ordered = T)
    
    alt_value<-raster::extract(dem,cbind(coords[,1],
                                         coords[,2]))
    
    den_value<-(1.023-0.0003*alt_value)*1000
-   biomass<-0.5*round(as.numeric(input$value)*((den_value*0.25)/1000),2)
+   biomass_ha<-(0.5*round(as.numeric(500)*(den_value*0.25),2)/1000)
    
-   biomass_df <- data.frame(
-    category=c("Bosque Natural", "Aguacate"),
-    count=c(96,biomass))
+   current_biomass<-biomass_ha*area
+  
    
-   biomass_df$ymax<-cumsum(biomass_df$count)[1:2]
-   biomass_df$ymin<-c(0,96)
-   biomass_df$labelPosition <- (biomass_df$ymax + biomass_df$ymin) / 2
-   biomass_df$label <- paste0(biomass_df$category,":",
-                              "\n", biomass_df$count)
+   forest_area<-(sum(values(kc)==1.25,na.rm = T)*(10^2))/10000
    
-   g<-ggplot(biomass_df, aes(ymax=ymax, ymin=ymin, xmax=4, 
-                       xmin=3, fill=category)) +
-    geom_rect() +
-    geom_label( x=3.5, aes(y=labelPosition, label=label), 
-                size=4) +
-    ggtitle("Carbono")+
-    coord_polar(theta="y")+
-    scale_fill_brewer(name = "",type = "qual",palette = "Greens")+
-    xlim(c(2, 4))+
-    theme_void()+
-    theme(
-     legend.position = "none")
+   biomass_sim<-biomass_ha*(area+forest_area*sim_values)
+   
+   forest_biomass<-(forest_area*60)
+   
+   forest_sim<-(forest_area-forest_area*sim_values)*60
+   
+   
+   biomass_df<-data.frame(cat = c("Aguacate","Bosque","Aguacate","Bosque"),
+                          biomasa = c(current_biomass,forest_biomass,
+                                      biomass_sim,
+                                      forest_sim),
+                          level = c("Actual","Actual","sim","sim"))
+   
+   biomass_df$level<-factor(biomass_df$level,
+                            levels = unique(biomass_df$level),
+                            labels = c("Actual",paste0(input$nsim,"%")))
+   
+   
+   g<-ggplot(biomass_df, aes(x = cat,y = biomasa))+
+    geom_bar(stat = "identity",col = "black",
+             fill = "gray80")+
+    ggtitle("Carbono almacenado")+
+    xlab("Cobertura")+
+    ylab("Carbono (Mg)")+
+    facet_wrap(~level,nrow = 2,ncol = 2)+
+    scale_y_continuous(
+     labels = function(x) format(x, scientific = FALSE))+
+    theme_bw()+
+    theme(panel.grid = element_blank(),
+          legend.position = "top")
      
-   
-   df$p_ratio<-df$prec_accum/1000
-   df$etp_ratio<-df$etp_accum/1000
-   
    g1<-ggplot(data = df)+
     geom_bar(aes(x = month,y = water_balance),
-             position = "dodge",stat = "identity",col = "black",fill = "gray80")+
-    geom_segment(aes(y = prec_accum-80,yend = prec_accum-80,
-                     x = -1*etp_ratio+month,xend = month,
-                     group = type),
-                 stat = "identity",col = "red",linewidth = 1.5)+
-    geom_segment(aes(y = prec_accum-80,yend = prec_accum-80,
-                     x = p_ratio+month,xend = month,
-                     group = type),
-                 stat = "identity",col = "blue",linewidth = 1.5)+
+             position = "dodge",stat = "identity",col = "black",
+             fill = "gray80")+
+    ggtitle("Balance hídrico")+
     scale_x_continuous(breaks = seq(1,12))+
     scale_linetype(name = "")+
    facet_wrap(~type,nrow = 2,ncol = 2)+
@@ -245,12 +297,14 @@ shinyApp(
    g1
    
    g1/g
+   
   }
   )
   
-  #output$myplot <- renderPlot({
-   #invest_model()
-   #})
+  output$myplot <- renderPlot({
+   g<-invest_model()
+   g
+   },res = 96)
   
   output$map<-renderLeaflet({
    points_df<-read.csv2("results_yield.csv")
